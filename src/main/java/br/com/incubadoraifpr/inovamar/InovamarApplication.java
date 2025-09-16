e7e628bf7e681fde22872d5b478d5d13e575ef4a
mitugui <cristian.mitugui04@gmail.com>
2025-09-16 14:43:06 -0300

    chore: initialize Spring Boot project with base setup and Docker support - Bootstrapped project using Spring Boot 3.5.5 with Java 17. - Added core dependencies:   - spring-boot-starter-web → REST API development.   - spring-boot-starter-data-jpa → persistence layer with JPA/Hibernate.   - spring-boot-starter-validation → request validation (Jakarta Validation).   - spring-boot-devtools → hot reload during development.   - lombok → boilerplate reduction with annotations.   - flyway-core & flyway-mysql → database migrations management.   - mysql-connector-j → runtime MySQL driver. - Added Dockerfile to build and run the application in a container: - Configured application.yml, externalized database properties via .env support.

---
+++ src/main/java/br/com/incubadoraifpr/inovamar/InovamarApplication.java
@@ -0,0 +1,12 @@
+package br.com.incubadoraifpr.inovamar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovamarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InovamarApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/incubadoraifpr/inovamar/InovamarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.incubadoraifpr.inovamar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InovamarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
